dc42e0cab3d89b8791c1cf1f6e3c9abca9d94376
kevinlucchese94 <kevinlucchese94@gmail.com>
2023-01-17 13:01:15 +0100

    test

---
+++ roles/scripts/templates/rsync_foto.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+#RSYNC DEI VIDEO DA SISTEMARE
+remote_dir="/home/kevin/foto_e_video/Foto/Da sistemare/"
+local_dir="/home/kevin/Video/"
+
+rsync -zvrah --delete \
+  --include="*.[Mm][Oo][Vv]" \
+  --include="*.[Mm][Pp][4]" \
+  --include="*.MOD" \
+  --exclude="*.[Jj][Pp][Gg]" \
+  --exclude="*.[Jj][Pp][Ee][Gg]" \
+  --exclude="*.[Pp][Nn][Gg]" \
+  --exclude="*.HEIC"  \
+  "$remote_dir" "$local_dir" 
+
+#RSYNC DEI VIDEO 
+remote_dir="/home/kevin/foto_e_video/Video"
+local_dir="/home/kevin/"
+
+rsync -zvrah --delete \
+  --include="*.[Mm][Oo][Vv]" \
+  --include="*.[Mm][Pp][4]" \
+  --include="*.MOD" \
+  --exclude="*.[Jj][Pp][Gg]" \
+  --exclude="*.[Jj][Pp][Ee][Gg]" \
+  --exclude="*.[Pp][Nn][Gg]" \
+  --exclude="*.HEIC"  \
+  "$remote_dir" "$local_dir" 
+
+#RSYNC DELLE FOTO
+remote_dir="/home/kevin/foto_e_video/Foto"
+local_dir="/home/kevin/"
+
+rsync -zvrah --delete \
+  --include="*.[Jj][Pp][Gg]" \
+  --include="*.[Pp][Nn][Gg]" \
+  --include="*.HEIC" \
+  --include="*.[Jj][Pp][Ee][Gg]" \
+  --exclude="*.MOD" \
+  --exclude="*.[Mm][Oo][Vv]" \
+  --exclude="*.[Mm][Pp]4" \
+  "$remote_dir" "$local_dir" 
