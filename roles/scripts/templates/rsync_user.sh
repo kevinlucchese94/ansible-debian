a879af8d6cf5c99ad644c879523574ab1ab1c1e0
Kevin Lucchese <78358512+kevinlucchese94@users.noreply.github.com>
2023-01-20 16:19:29 +0100

    Update rsync_user.sh

--- roles/scripts/templates/rsync_user.sh
+++ roles/scripts/templates/rsync_user.sh
@@ -1,43 +1,98 @@
-#!/bin/sh
-bash /home/kevin/scripts/mount_samba.sh
-#RSYNC DEI VIDEO 
-remote_dir="/home/kevin/foto_e_video/Video"
-local_dir="/home/kevin/"
+#!/bin/bash
 
-rsync -zvrah --delete \
-  --include="*.[Mm][Oo][Vv]" \
-  --include="*.[Mm][Pp][4]" \
-  --include="*.MOD" \
-  --exclude="*.[Jj][Pp][Gg]" \
-  --exclude="*.[Jj][Pp][Ee][Gg]" \
-  --exclude="*.[Pp][Nn][Gg]" \
-  --exclude="*.HEIC"  \
-  "$remote_dir" "$local_dir" 
+# controlla se il disco samba è già montato
+if grep -qs '/home/kevin/foto_e_video' /proc/mounts; then
+  echo "Il disco samba è già montato."
+  # impostazione delle cartelle di origine e destinazione
+  #RSYNC DEI VIDEO
+  remote_dir="/home/kevin/foto_e_video/Video"
+  local_dir="/home/kevin/"
 
-#RSYNC DEI VIDEO DA SISTEMARE
-remote_dir="/home/kevin/foto_e_video/Foto/Da sistemare/"
-local_dir="/home/kevin/Video/Da sistemare/"
+  rsync -zvrah --delete \
+    --include="*.[Mm][Oo][Vv]" \
+    --include="*.[Mm][Pp][4]" \
+    --include="*.MOD" \
+    --exclude="*.[Jj][Pp][Gg]" \
+    --exclude="*.[Jj][Pp][Ee][Gg]" \
+    --exclude="*.[Pp][Nn][Gg]" \
+    --exclude="*.HEIC" \
+    "$remote_dir" "$local_dir"
 
-rsync -zvrah --delete \
-  --include="*.[Mm][Oo][Vv]" \
-  --include="*.[Mm][Pp][4]" \
-  --include="*.MOD" \
-  --exclude="*.[Jj][Pp][Gg]" \
-  --exclude="*.[Jj][Pp][Ee][Gg]" \
-  --exclude="*.[Pp][Nn][Gg]" \
-  --exclude="*.HEIC"  \
-  "$remote_dir" "$local_dir" 
+  #RSYNC DEI VIDEO DA SISTEMARE
+  remote_dir="/home/kevin/foto_e_video/Foto/Da sistemare/"
+  local_dir="/home/kevin/Video/Da sistemare/"
 
-#RSYNC DELLE FOTO
-remote_dir="/home/kevin/foto_e_video/Foto"
-local_dir="/home/kevin/"
+  rsync -zvrah --delete \
+    --include="*.[Mm][Oo][Vv]" \
+    --include="*.[Mm][Pp][4]" \
+    --include="*.MOD" \
+    --exclude="*.[Jj][Pp][Gg]" \
+    --exclude="*.[Jj][Pp][Ee][Gg]" \
+    --exclude="*.[Pp][Nn][Gg]" \
+    --exclude="*.HEIC" \
+    "$remote_dir" "$local_dir"
 
-rsync -zvrah --delete \
-  --include="*.[Jj][Pp][Gg]" \
-  --include="*.[Pp][Nn][Gg]" \
-  --include="*.HEIC" \
-  --include="*.[Jj][Pp][Ee][Gg]" \
-  --exclude="*.MOD" \
-  --exclude="*.[Mm][Oo][Vv]" \
-  --exclude="*.[Mm][Pp]4" \
-  "$remote_dir" "$local_dir" 
+  #RSYNC DELLE FOTO
+  remote_dir="/home/kevin/foto_e_video/Foto"
+  local_dir="/home/kevin/"
+
+  rsync -zvrah --delete \
+    --include="*.[Jj][Pp][Gg]" \
+    --include="*.[Pp][Nn][Gg]" \
+    --include="*.HEIC" \
+    --include="*.[Jj][Pp][Ee][Gg]" \
+    --exclude="*.MOD" \
+    --exclude="*.[Mm][Oo][Vv]" \
+    --exclude="*.[Mm][Pp]4" \
+    "$remote_dir" "$local_dir"
+else
+  # monta il disco samba sulla cartella specificata
+  sudo mount -t cifs -o username=pi,password=Erminio67,rw //192.168.5.198/foto_e_video /home/kevin/foto_e_video
+  if [ $? -eq 0 ]; then
+    echo "Il disco samba è stato montato con successo."
+    #RSYNC DEI VIDEO
+    remote_dir="/home/kevin/foto_e_video/Video"
+    local_dir="/home/kevin/"
+
+    rsync -zvrah --delete \
+      --include="*.[Mm][Oo][Vv]" \
+      --include="*.[Mm][Pp][4]" \
+      --include="*.MOD" \
+      --exclude="*.[Jj][Pp][Gg]" \
+      --exclude="*.[Jj][Pp][Ee][Gg]" \
+      --exclude="*.[Pp][Nn][Gg]" \
+      --exclude="*.HEIC" \
+      "$remote_dir" "$local_dir"
+
+    #RSYNC DEI VIDEO DA SISTEMARE
+    remote_dir="/home/kevin/foto_e_video/Foto/Da sistemare/"
+    local_dir="/home/kevin/Video/Da sistemare/"
+
+    rsync -zvrah --delete \
+      --include="*.[Mm][Oo][Vv]" \
+      --include="*.[Mm][Pp][4]" \
+      --include="*.MOD" \
+      --exclude="*.[Jj][Pp][Gg]" \
+      --exclude="*.[Jj][Pp][Ee][Gg]" \
+      --exclude="*.[Pp][Nn][Gg]" \
+      --exclude="*.HEIC" \
+      "$remote_dir" "$local_dir"
+
+    #RSYNC DELLE FOTO
+    remote_dir="/home/kevin/foto_e_video/Foto"
+    local_dir="/home/kevin/"
+
+    rsync -zvrah --delete \
+      --include="*.[Jj][Pp][Gg]" \
+      --include="*.[Pp][Nn][Gg]" \
+      --include="*.HEIC" \
+      --include="*.[Jj][Pp][Ee][Gg]" \
+      --exclude="*.MOD" \
+      --exclude="*.[Mm][Oo][Vv]" \
+      --exclude="*.[Mm][Pp]4" \
+      "$remote_dir" "$local_dir"
+  else
+    echo "Si è verificato un errore nel montare il disco samba."
+  fi
+fi
+
