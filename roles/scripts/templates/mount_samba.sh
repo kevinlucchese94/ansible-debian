44f7f56f7b2876968cfef940c03be07064614f37
Kevin Lucchese <78358512+kevinlucchese94@users.noreply.github.com>
2023-01-20 16:12:52 +0100

    Update mount_samba.sh

--- roles/scripts/templates/mount_samba.sh
+++ roles/scripts/templates/mount_samba.sh
@@ -4,13 +4,11 @@
 if grep -qs '/home/kevin/foto_e_video' /proc/mounts; then
   echo "Il disco samba è già montato."
     # impostazione delle cartelle di origine e destinazione
-  bash rsync_user.sh
 else
   # monta il disco samba sulla cartella specificata
   sudo mount -t cifs -o username=pi,password=Erminio67,rw //192.168.5.198/foto_e_video /home/kevin/foto_e_video
   if [ $? -eq 0 ]; then
     echo "Il disco samba è stato montato con successo."
-    bash rsync_user.sh
   else
     echo "Si è verificato un errore nel montare il disco samba."
   fi
