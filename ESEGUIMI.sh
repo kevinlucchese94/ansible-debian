5e26edb5aa8a9db090f611d3ab9388bdf9bf7db7
Kevin Lucchese <78358512+kevinlucchese94@users.noreply.github.com>
2023-09-14 12:46:22 +0200

    Update ESEGUIMI.sh

--- ESEGUIMI.sh
+++ ESEGUIMI.sh
@@ -10,9 +10,15 @@ git clone https://github.com/kevinlucchese94/ansible-debian.git
 # Enter into the downloaded directory
 cd ansible-debian
 
+# Install Docker (if not already installed)
+if ! command -v docker &> /dev/null; then
+    curl -sSL https://get.docker.com | sh
+    sudo usermod -aG docker pi
+fi
+
 #install Portainer
 docker volume create portainer_data
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
 # Run the ansible playbook
-ansible-playbook main.yml with --ask-become-pass 
+ansible-playbook main.yml --ask-become-pass
