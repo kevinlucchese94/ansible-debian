e0e44ccb103090d38554ed11b7ffb725b3725632
Kevin Lucchese <78358512+kevinlucchese94@users.noreply.github.com>
2023-11-21 14:22:28 +0100

    Update rsync_root.sh

--- roles/scripts/templates/rsync_root.sh
+++ roles/scripts/templates/rsync_root.sh
@@ -1,68 +1,52 @@
 #!/bin/bash
 
-# controlla se il disco samba è già montato
-if grep -qs '/home/kevin/foto_e_video' /proc/mounts; then
-  echo "Il disco samba è già montato."
-  # impostazione delle cartelle di origine e destinazione
-  #RSYNC DEI VIDEO
-  remote_dir="/home/kevin/foto_e_video/"
-  local_dir="/home/kevin/Video"
-
+# Funzione per eseguire il RSYNC
+function perform_rsync_video {
   rsync -zvrah --delete \
-    --include="*.[Mm][Oo][Vv]" \
-    --include="*.[Mm][Pp][4]" \
-    --include="*.MOD" \
     --exclude="*.[Jj][Pp][Gg]" \
-    --exclude="*.[Jj][Pp][Ee][Gg]" \
     --exclude="*.[Pp][Nn][Gg]" \
     --exclude="*.HEIC" \
-    "$local_dir" "$remote_dir"
-
-  #RSYNC DELLE FOTO
-  remote_dir="/home/kevin/foto_e_video/"
-  local_dir="/home/kevin/Foto"
-
+    --exclude="*.[Jj][Pp][Ee][Gg]" \
+    "$2" "$1"
+}
+# Funzione per eseguire il RSYNC
+function perform_rsync_foto {
   rsync -zvrah --delete \
-    --include="*.[Jj][Pp][Gg]" \
-    --include="*.[Pp][Nn][Gg]" \
-    --include="*.HEIC" \
-    --include="*.[Jj][Pp][Ee][Gg]" \
-    --exclude="*.MOD" \
     --exclude="*.[Mm][Oo][Vv]" \
-    --exclude="*.[Mm][Pp]4" \
-    "$local_dir" "$remote_dir"
+    --exclude="*.[Mm][Pp][4]" \
+    --exclude="*.MOD" \
+    --exclude="*.[Aa][Vv][Ii]" \
+    --exclude="*.[Mm][Kk][Vv]" \
+    --exclude="*.[Ww][Mm][Vv]" \
+    --exclude="*.[Ff][Ll][Vv]" \
+    --exclude="*.[Mm][Pp][Gg]" \
+    --exclude="*.[Ww][Ee][Bb][Mm]" \
+    "$2" "$1"
+}
+# Cartella remota principale
+remote_base="/home/kevin/foto_e_video"
+
+# controlla se il disco samba è già montato
+if grep -qs "$remote_base" /proc/mounts; then
+  echo "Il disco samba è già montato."
+
+  # RSYNC DEI VIDEO
+  perform_rsync_video "$remote_base/ALL/" "/home/kevin/Video"
+
+  # RSYNC DELLE FOTO
+  perform_rsync_foto "$remote_base/ALL/" "/home/kevin/Foto"
 else
   # monta il disco samba sulla cartella specificata
   sudo mount -t cifs -o username=pi,password=Erminio67,rw //192.168.5.198/foto_e_video /home/kevin/foto_e_video
+
   if [ $? -eq 0 ]; then
     echo "Il disco samba è stato montato con successo."
-    #RSYNC DEI VIDEO
-    remote_dir="/home/kevin/foto_e_video/"
-    local_dir="/home/kevin/Video"
 
-    rsync -zvrah --delete \
-      --include="*.[Mm][Oo][Vv]" \
-      --include="*.[Mm][Pp][4]" \
-      --include="*.MOD" \
-      --exclude="*.[Jj][Pp][Gg]" \
-      --exclude="*.[Jj][Pp][Ee][Gg]" \
-      --exclude="*.[Pp][Nn][Gg]" \
-      --exclude="*.HEIC" \
-      "$local_dir" "$remote_dir"
+  # RSYNC DEI VIDEO
+  perform_rsync_video "$remote_base/ALL/" "/home/kevin/Video"
 
-    #RSYNC DELLE FOTO
-    remote_dir="/home/kevin/foto_e_video/"
-    local_dir="/home/kevin/Foto"
-
-    rsync -zvrah --delete \
-      --include="*.[Jj][Pp][Gg]" \
-      --include="*.[Pp][Nn][Gg]" \
-      --include="*.HEIC" \
-      --include="*.[Jj][Pp][Ee][Gg]" \
-      --exclude="*.MOD" \
-      --exclude="*.[Mm][Oo][Vv]" \
-      --exclude="*.[Mm][Pp]4" \
-      "$local_dir" "$remote_dir"
+  # RSYNC DELLE FOTO
+  perform_rsync_foto "$remote_base/ALL/" "/home/kevin/Foto"
   else
     echo "Si è verificato un errore nel montare il disco samba."
   fi
