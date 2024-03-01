1e7d698def5809400db082aaba853027914649da
Kevin Lucchese <78358512+kevinlucchese94@users.noreply.github.com>
2024-03-01 17:44:40 +0100

    Update ESEGUIMI.sh

--- ESEGUIMI.sh
+++ ESEGUIMI.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Richiedi il nome utente
-read -p "Inserisci il nome utente: " utente
-# Richiedi la password
-read -s -p "Inserisci la password: " password
+read -p "Inserisci il nome utente per il nuovo account: " nuovo_utente
+# Richiedi la password per il nuovo utente
+read -s -p "Inserisci la password per il nuovo utente: " nuova_password
 echo
 
 # Installa Git e Ansible
@@ -15,8 +15,9 @@ git clone https://github.com/kevinlucchese94/ansible-debian.git
 
 # Entra nella directory scaricata
 cd ansible-debian
+
 # Salva il nome utente e la password nel file vars.yml
-echo -e "utente: $utente\npassword: $password" > vars.yml
+echo -e "utente: $utente\npassword: $password\nnuovo_utente: $nuovo_utente\nnuova_password: $nuova_password" > vars.yml
 
 # Crea il file hosts
 echo "[target]" > hosts
@@ -27,7 +28,3 @@ echo -e "[defaults]\nremote_user = $utente" > ansible.cfg
 
 # Esegui il playbook Ansible
 ansible-playbook main.yml --ask-become-pass
-
-
-# Run the ansible playbook
-ansible-playbook ansible-debian/main.yml --ask-become-pass
