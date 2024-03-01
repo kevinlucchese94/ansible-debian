6543212027bef7e1291cd99b6eb9530b91c73e5c
Kevin Lucchese <78358512+kevinlucchese94@users.noreply.github.com>
2024-03-01 17:38:47 +0100

    Update ESEGUIMI.sh

--- ESEGUIMI.sh
+++ ESEGUIMI.sh
@@ -1,40 +1,33 @@
 #!/bin/bash
 
-# Richiedi il nome
-read -p "Inserisci il nome: " nome
+# Richiedi il nome utente
+read -p "Inserisci il nome utente: " utente
 # Richiedi la password
 read -s -p "Inserisci la password: " password
+echo
 
-# Install Git and Ansible
-apt-get update
-apt-get install -y git ansible
+# Installa Git e Ansible
+sudo apt-get update
+sudo apt-get install -y git ansible
 
 # Clone ansible-debian repository
 git clone https://github.com/kevinlucchese94/ansible-debian.git
 
-# Enter into the downloaded directory
+# Entra nella directory scaricata
 cd ansible-debian
-# Salva il nome, cognome e password nel file vars.yml
-echo -e "nome: $nome\npassword: $password" > vars.yml
+# Salva il nome utente e la password nel file vars.yml
+echo -e "utente: $utente\npassword: $password" > vars.yml
 
-##creo il file hosts e vars
-echo "[target]" > hosts ##serve per ansible
-echo "localhost" >> hosts ##serve per ansible
-echo "[target:vars]" >> hosts ##serve per ansible
-echo "ansible_ssh_user=utente" >> hosts ##serve per ansible
-echo "ansible_ssh_pass=4" >> hosts ##serve per ansible
-echo "ansible_python_interpreter=/usr/bin/python3" >> hosts
-echo "ansible_sudo_pass=4" >> hosts
+# Crea il file hosts
+echo "[target]" > hosts
+echo "localhost" >> hosts
 
-# Install Docker (if not already installed)
-if ! command -v docker &> /dev/null; then
-    curl -sSL https://get.docker.com | sh
-    sudo usermod -aG docker pi
-fi
+# Crea il file ansible.cfg per specificare l'utente sudo
+echo -e "[defaults]\nremote_user = $utente" > ansible.cfg
 
-#install Portainer
-docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+# Esegui il playbook Ansible
+ansible-playbook main.yml --ask-become-pass
+
 
 # Run the ansible playbook
 ansible-playbook ansible-debian/main.yml --ask-become-pass
